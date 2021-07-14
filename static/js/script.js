0c53f9c6b6a3c4694736effe427aeed5523d422d
Evka K <kuklaewa@gmail.com>
2021-07-14 10:30:11 +0000

    Add Materialize Navbar and Sidenav

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+// Mobile Collapse Initialization from materializecss
+$(document).ready(function(){
+    $(".sidenav").sidenav();
+  });
+
+
+
+// Dropdown menu on click trigger from materializecss
+  $(".dropdown-trigger").dropdown();
