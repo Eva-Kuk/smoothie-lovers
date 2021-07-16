3a69d19cb09ac92adc62385616b7ec69c44a72bc
Evka K <kuklaewa@gmail.com>
2021-07-16 11:39:52 +0000

    Add new properties and style image card

--- app.py
+++ app.py
@@ -98,6 +98,7 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
