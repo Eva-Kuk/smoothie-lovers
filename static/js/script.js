0a721f8a6d282766593179141a21744b88065758
Evka K <kuklaewa@gmail.com>
2021-07-18 14:43:04 +0000

    Add custom Materialize select validation

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,40 @@
-// Initialization from materializecss
+// jQuerry Initialization from materializecss
 $(document).ready(function(){
     $(".sidenav").sidenav();
     $(".dropdown-trigger").dropdown();
     $('.tooltipped').tooltip();
     $('select').formSelect();
-  });
 
 
+// CREDIT: Code taken from Miniproject Materialize Validation
+  validateMaterializeSelect();
+  function validateMaterializeSelect() {
+      let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+      let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+      if ($("select.validate").prop("required")) {
+          $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+      }
+      $(".select-wrapper input.select-dropdown").on("focusin", function () {
+          $(this).parent(".select-wrapper").on("change", function () {
+              if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                  $(this).children("input").css(classValid);
+              }
+          });
+      }).on("click", function () {
+          if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+              $(this).parent(".select-wrapper").children("input").css(classValid);
+          } else {
+              $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                  if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                      if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                          $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                      }
+                  }
+              });
+          }
+      });
+  }
+});
 
 // Adding list item to the ingredients list in add_recipe page
 $( "#ingredients .add-list-item" ).click(function(event) {
@@ -36,3 +64,4 @@ $( "#method .add-list-item" ).click(function(event) {
                   </li>`;
   $( this ).parent().before(listItem);
 });
+
