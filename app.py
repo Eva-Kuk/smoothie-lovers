a1f3344e73981972d402397a5cea06b5a33fa519
Evka K <kuklaewa@gmail.com>
2021-07-21 09:44:14 +0000

    feat: Add Manage Categories template

--- app.py
+++ app.py
@@ -153,6 +153,12 @@ def delete_recipe(recipe_id):
     return redirect(url_for("get_recipes"))
 
 
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("name", 1))
+    return render_template("categories.html", categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
