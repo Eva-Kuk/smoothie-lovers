cedbe7191421df770b3e51119dffccee084c9952
Evka K <kuklaewa@gmail.com>
2021-07-28 09:25:07 +0000

    Remove pagination import

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from flask_paginate import Pagination, get_page_args
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
