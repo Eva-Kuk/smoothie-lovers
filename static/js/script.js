0c0458a3166fd9b97a689d6e17e6707c02bcb9d6
Evka K <kuklaewa@gmail.com>
2021-08-04 00:24:11 +0000

    Add change btn class name

--- static/js/script.js
+++ static/js/script.js
@@ -38,31 +38,30 @@ $(document).ready(function(){
 });
 
 // Adding list item to the ingredients list in add_recipe page
-$( "#ingredients .add-list-item" ).click(function(event) {
+$( "#ingredients .green-btn" ).click(function(e) {
   let listItem = `<li class='collection-item'>
                     <div class='input-field'>
                       <input name='ingredients' type='text' maxlength='120' required>
                     </div>
-                    <a class="btn-floating btn-small remove-list-item">
+                    <a class="btn-floating btn-small red-btn">
                     <i class="fas fa-minus"></i></a>
                   </li>`;
   $( this ).parent().before(listItem);
 });
   
 // Removing list item from the  ingredients list and steps in add_recipe page
-$( "#method, #ingredients").on("click", ".remove-list-item", function(event) {
+$( "#method, #ingredients").on("click", ".red-btn", function(e) {
   $( this ).parent().remove();
 });
 
 // Adding a list item to the method list
-$( "#method .add-list-item" ).click(function(event) {
+$( "#method .green-btn" ).click(function(e) {
   let listItem =  `<li class='collection-item'>
                     <div class='input-field'>
                     <input name='method' type='text' maxlength='120' required>
                     </div>
-                    <a class="btn-floating btn-small remove-list-item">
+                    <a class="btn-floating btn-small red-btn">
                     <i class="fas fa-minus"></i></a>
                   </li>`;
   $( this ).parent().before(listItem);
 });
-
