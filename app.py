f2d157dc028efbf9abd235d14345dd77e940af00
Evka K <kuklaewa@gmail.com>
2022-11-20 19:05:12 +0000

    change settings

--- app.py
+++ app.py
@@ -447,4 +447,4 @@ def internal_server_error(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
